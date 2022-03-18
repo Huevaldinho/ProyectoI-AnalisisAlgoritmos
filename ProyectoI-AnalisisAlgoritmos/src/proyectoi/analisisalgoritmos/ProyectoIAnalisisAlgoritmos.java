c8d7c58063541379b6a1c73bde4378aea680e474
Huevaldinho <47650141+Huevaldinho@users.noreply.github.com>
2022-03-18 11:38:37 -0600

    Creacion de proyecto en Netbeans
    Solo hice el proyecto en  Netbeans

---
+++ ProyectoI-AnalisisAlgoritmos/src/proyectoi/analisisalgoritmos/ProyectoIAnalisisAlgoritmos.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyectoi.analisisalgoritmos;
+
+/**
+ *
+ * @author Usuario
+ */
+public class ProyectoIAnalisisAlgoritmos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+    }
+    
+}
