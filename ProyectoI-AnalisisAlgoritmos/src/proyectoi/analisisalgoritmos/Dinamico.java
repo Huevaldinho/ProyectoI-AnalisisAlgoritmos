27e157264d767e1d6b3dee8dff35beaac2d50ea7
Huevaldinho <47650141+Huevaldinho@users.noreply.github.com>
2022-03-30 16:21:03 -0600

    Limpie un poco la clase
    Corregi unos errores que tenia pero aun falta el caso donde no existe subconjunto que sumado de 0 entonces hay que tomar el mas cercano

--- ProyectoI-AnalisisAlgoritmos/src/proyectoi/analisisalgoritmos/Dinamico.java
+++ ProyectoI-AnalisisAlgoritmos/src/proyectoi/analisisalgoritmos/Dinamico.java
@@ -3,29 +3,35 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package proyectoi.analisisalgoritmos;
-
-import static java.lang.Math.abs;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
+import static java.lang.Math.abs;
+import java.util.stream.IntStream;
 
 /**
  *
- * @author Usuario
+ * @author Felipe Obando y Sophya
  */
-public class Dinamico {
-        
+public class Dinamico{ // PRUEBAS DE COMBINACIONES DE CODIGO PARA LLEGAR AL DINAMICO
     //Suma de todos los numeros positivos.
-    private static int sumaPositivos=0;
+    private static int sumaPositivos;
     //Suma de todos los numeros negativos.
-    private static int sumaNegativos=0;
+    private static int sumaNegativos;
     //Valores de las columnas segun la suma de positivos y negativos.
-    private static HashMap<Integer, Integer> valorColumna = new HashMap<>(); 
+    private static HashMap<Integer, Integer> valorColumna;
     //Subconjunto que  sumado da 0
-    private static List<Integer> subconjunto = new ArrayList<>();
-    
+    private static List<Integer> subconjunto;
+
+    public Dinamico() {
+        sumaPositivos=0;
+        sumaNegativos=0;
+        valorColumna= new HashMap<>();
+        subconjunto = new ArrayList<>();
+    }
     /**
-     * 
+     * Metodo para sumar todos los negativos y todos los positivos.
      * @param array: Array de numeros enteros.
      */
     static void sumarNumeros(int array[]){
@@ -35,61 +41,53 @@ public class Dinamico {
             else
                sumaNegativos+=array[i];
         }
-        System.out.println("Positivos: "+sumaPositivos+" Negativos: "+sumaNegativos);
-    }/**
-     * 
-     * @param array: Arreglo de enteros a imprimir
-     */
-    static void imprimirArreglo(int array[]){
-        System.out.println("Arreglo ingresado: ");
-        for (int i=0;i<array.length;i++)
-            System.out.println(array[i]);
     }
     /**
-     * 
+     * Metodo para imprimir la matriz generada.
      * @param array: Array de numeros
      * @return int[][]: Matriz de tamano: filas = array.lenght y columnas = suma de todos los positivos + abs(suma de todos los negativos) +1 (cero).
      */
     static void imprimirMatriz(boolean [][]matriz,int array[]){
-        //Imprimir tamano matriz.
         for (int i=0;i<matriz.length;i++){
             for (int j=0;j<matriz[i].length;j++){
                 System.out.println("Valores: "+array[i]+" y "+valorColumna.get(j));
                 System.out.println("Fila "+i+" - Columna: "+j+" es: "+matriz[i][j]);
-                //System.out.print(matriz[i][j]+"     ");
                 
             }
             System.out.println("");
         }
     }
     /**
-     * 
+     * Metodo para asignar el valor de cada columna segun las sumas de positivos y negativos
      * @param columnas: Cantidad de columnas de la matriz.
      */
     static void asignarValoresColumnas(int columnas){
-        //System.out.println("Cantidad de columnas: "+columnas);
-        int p=1;//Variable para llevar contador de 1 hasta sumaPositivos
+        int p=1;//Variable para llevar contador de 1 hasta sumaPositivos para que la matriz asigne valores de menor a mayor
         for (int i = 0; i < columnas; i++){
             if (sumaNegativos<=0){
                 valorColumna.put(i, sumaNegativos);
                 sumaNegativos++;
-            }else if (sumaNegativos==0){
-                valorColumna.put(i,0);
-            }else if(p<=sumaPositivos){
+            }else if(0<sumaPositivos){
                 valorColumna.put(i,p);
                 p++;
             }
-            
         }
-        System.out.println(valorColumna);
     }
     /**
-     * 
+     * Dado un conjunto de enteros, encontrar subconjunto que sumado de cero.
      * @param array: Arreglo donde se buscara el subconjunto que sumado de cero.
      */
-    static void subsetSum(int array[]){
-        //Arrays.sort(array);
-        //imprimirArreglo(array);
+    public static void subsetSum(int array[]){
+        if (array.length==0){
+            System.out.println(subconjunto);
+            return;
+        }
+        //Incicia variables
+        sumaPositivos=0;
+        sumaNegativos=0;
+        valorColumna = new HashMap<>(); 
+        subconjunto = new ArrayList<>();
+        Arrays.sort(array);//Lo ordena y toma el mas menor
         sumarNumeros(array);
         //Si no hay negativos, tome el menor de los positivos (el arreglo esta ordenado de menor a mayor)
         if (sumaNegativos==0){
@@ -100,25 +98,23 @@ public class Dinamico {
             //Crear matriz
             int numeroColumnas = sumaPositivos+abs(sumaNegativos)+1;//Cantidad de columnas necesarias
             boolean [][] matriz = new boolean[array.length][numeroColumnas];//Creacion de matriz
-            //imprimirMatriz(matriz);//Solo para ver si estan las columnas y filas que deberian estar
             //Asignar valores de columnas en el HashMap
             asignarValoresColumnas(numeroColumnas);
-            //Hasta este punto tengo, matriz creada, columnas con valor entonces estoy
-            //listo para hacer todo el proceso
+            //Empieza el algoritmo
             conseguirSubconjunto(array,matriz,numeroColumnas);
         
         }
-        //System.out.println("Subconjunto: "+subconjunto);
-    }
-    static boolean puedoSumar(int valorColumna,int []arrayEnRango){
-        
-        
-        return false;
-    }
-    static boolean conseguirSubconjunto(int array[],boolean [][] matriz,int columnasTotales){
+        System.out.println(subconjunto);
+    }/**
+     *  Metodo para conseguir el subconjunto utilizando programacion dinamica
+     * almacena resultados en una matriz que utiliza para no repetir lo mismo varias veces.
+     * @param array: Array del conjunto base
+     * @param matriz: Matriz donde se guardan resultados
+     * @param columnasTotales: Numero total de columnas de matriz.
+     */
+    static void conseguirSubconjunto(int array[],boolean [][] matriz,int columnasTotales){
         int columnaBuscada=0;
-        //imprimirMatriz(matriz);
-        int primeraFilaRevisar=0;
+        //La primera fila se trata por aparte porque no se puede subir una fila como en las demas.
         for (int k=0;k<matriz[0].length;k++){
             if (valorColumna.get(k)==array[0]){
                 matriz[0][k]=true;
@@ -130,12 +126,11 @@ public class Dinamico {
                 //La primera fila se trata diferente porque no se puede subir y da error de index
                 //Valor de columna == valor del arreglo en el index de la fila
                 if (valorColumna.get(columna)==array[fila]){
-                    matriz[fila][columna]=true;
+                    matriz[fila][columna] = true;
                     continue;
                 }
                 //key es el index de las columnas
                 for (int key : valorColumna.keySet()){//Llaves del hashmap
-                    //System.out.println(key);
                     //valorColumna.get(key) valor de esa columna
                     //si el valor es igual al resultado de : valor de columna - valor del array en index fila
                     if (valorColumna.get(key)==valorColumna.get(columna)-array[fila]){
@@ -146,36 +141,33 @@ public class Dinamico {
                 //Elemento array mayor
                 if(array[fila]>valorColumna.get(columna)){
                     matriz[fila][columna] = matriz[fila-1][columna]||matriz[fila-1][columnaBuscada];
-                }
-                //Elemento array menor
-                if (array[fila]<valorColumna.get(columna)){
-                    //Celda actual        = celda arriba            o  matriz en fila de arriba en columna= valor columna - valor array en index fila
+                }else{//Elemento array menor
+                    //Celda actual = celda arriba o matriz en fila de arriba en columna = valor columna - valor array en index fila
                     matriz[fila][columna] = matriz[fila-1][columna] || matriz[fila-1][columnaBuscada];            
                 }
-                
             }
         }
-        
-        imprimirMatriz(matriz,array);
-        //Retornar el valor que buscamos, o sea, el valor que tiene la columna con valor 0.
-        //Dependiendo de eso sabemos si existe o no subconjunto, si existe lo mandamos a sacar
+        //Lee matriz
         resultado(array,matriz);
-        return false;
     }
+    /**
+     * Metodo para leer si existe o no un subconjunto que sumado de 0.
+     * @param array: Arreglo del conjunto base.
+     * @param matriz: Matriz completa.
+     */
     static void resultado(int array[],boolean [][] matriz){
         int columna=0;
+        //Saca el resultado de la columna del 0.
         for (int key : valorColumna.keySet()){
-                if (valorColumna.get(key)==0){//Saca la columna del 0
-                    columna=key;
-                }
+            if (valorColumna.get(key)==0){//Saca la columna del 0
+                columna=key;
+            }
         }
         //Si no hay solucion salgase
         if (!matriz[matriz.length-1][columna])
             return;
         //Empieza desde la ultima fila
-        //Se hace mientras la fila sea mayor que 0
         //El ciclo sube de abajo hacia arriba la matriz
-        //System.out.println("key del cero: "+columna+" fila empieza: "+(matriz.length-1));
         for (int fila=matriz.length-1;fila>=0;fila--){
             if(fila==0){
                 if (matriz[fila][columna]){
@@ -183,30 +175,42 @@ public class Dinamico {
                 }
                 break;
             }
-            if (matriz[fila-1][columna]==true)//celda de arriba es true, solo subo
-                continue;
-            else if(!matriz[fila-1][columna]){//Celda de arriba es false
+            if(!matriz[fila-1][columna]){//Celda de arriba es false. Si la celda de arriba es true no se hace nada
                 subconjunto.add(array[fila]);//Agrego al elemento donde estoy parado en la fila
                 columna = valorColumna.get(columna) - array[fila];//Este es el valor de la columna dodne tengo que ir
-                System.out.println("Tengo que ir a la columna etiquetada: "+columna);
                 for (int key : valorColumna.keySet()){
                     if (valorColumna.get(key)==columna){//Saca la columna del 0
-                        System.out.println("Valor: "+valorColumna.get(key)+" - Columna: "+key);
                         columna=key;
                         break;
                     }
                 }
-            }
-            
-        }
-        System.out.println(subconjunto);
+                //Suma todos los elementos del subconjunto
+                //Esto es para asegurarnos que no entren mas datos que no deben
+                if (IntStream.of(subconjunto.stream().mapToInt(i -> i).toArray()).sum()==0)
+                    return;
+            }   
+        }   
     }
-    
-    
     public static void main(String[] args) {
-           int a[]={-15,-6,1,2,5,7};
-           //sumarNumeros(a);
-           subsetSum(a);
+        int [] a = {7,-7,3,5,2,1};
+        int [] b={-5,-3,-2,5,8};
+        int [] c = {9,-7,3,5,8,1};
+        int d[]={-15,-6,1,2,3,5,7};
+        int e[]={-650,-60,-73,-71,-3,50,125,25,7};
+
+        System.out.println("a");
+        subsetSum(a);
+        System.out.println("b");
+        subsetSum(b);
+        System.out.println("c");
+        subsetSum(c);
+        System.out.println("d");
+        subsetSum(d);
+        System.out.println("e");
+        subsetSum(e);
+
+        //Falta agregar el caso donde no existe solucion pero hay que dar el subconjunto mas cercano a 0
+           
         
     }
 }
